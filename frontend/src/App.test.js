255ffbae553efc102203fa90d449ca835e04aae5
kichang <kichang>
2020-11-14 16:11:51 +0900

    App.js testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { createMemoryHistory } from 'history';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
