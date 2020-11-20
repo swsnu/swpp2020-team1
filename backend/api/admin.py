f603cbfff5ad4822ce293c4fd4633b747a1d64bb
kichang <kichang>
2020-11-21 03:02:30 +0900

    implemented recipe, recipe comment backend api

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,12 @@
 ''' admin.py: register models '''
 
 from django.contrib import admin
-from .models import Category, Barcode, Item, ItemCount
+from .models import Category, Barcode, Item, ItemCount, Recipe, RecipeComment
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Barcode)
 admin.site.register(Item)
 admin.site.register(ItemCount)
+admin.site.register(Recipe)
+admin.site.register(RecipeComment)
